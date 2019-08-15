de853e1432bfa35663f6039e24d98f628677a04e
John Preston <wcerfgba@riseup.net>
2019-08-15 14:05:42 +0100

    Structuring Game a bit more

--- src/Game.ts
+++ src/Game.ts
@@ -1,6 +1,9 @@
+import { _if } from './Utils'
+
 export interface Game {
   grid: Grid,
-  players: Map<PlayerId, Player>,
+  players: Array<Player>,
+  tick: number,
 }
 
 // We build the Grid like an onion, so we can add extra Layers of Cells
@@ -36,8 +39,9 @@ export interface Layer {
 export type Wall = Array<Cell>
 
 export interface Cell {
-  player: Player,
-  value: Number,
+  previousOwner: Player,
+  owner: Player,
+  value: number,
 }
 
 export interface Player {
@@ -45,6 +49,7 @@ export interface Player {
   name: string,
   colour: string,
   location: Location,
+  ticksInLocation: number,
 }
 
 export type PlayerId = string
@@ -52,21 +57,78 @@ export type PlayerId = string
 export type Location = WallLocation | CornerLocation
 export type WallLocation = [LayerIndex, WallIndex, CellIndex]
 export type CornerLocation = [LayerIndex, CornerIndex]
-export type LayerIndex = Number
-export type WallIndex = 'n' | 'e' | 's' | 'w'
-export type CellIndex = Number
-export type CornerIndex = 'ne' | 'se' | 'sw' | 'nw'
+export type LayerIndex = number
+export enum WallIndex { n = 'n', e = 'e', s = 's', w = 'w' }
+export type CellIndex = number
+export enum CornerIndex { ne = 'ne', se = 'se', sw = 'sw', nw = 'nw' }
 
-export interface Update {
+const isWallLocation = (location: Location): location is WallLocation => location.length === 3
 
+export type Update = AddPlayerUpdate | SetCellUpdate | RemovePlayerUpdate
+
+interface BaseUpdate {
+  tick: number,
+  type: UpdateType,
 }
 
-export const create = (): Game => ({})
-
-export const update = (updates: Array<Update>): Game => ({})
-
-
-
-const addPlayer = (g: Game): Game => {
-  
+enum UpdateType {
+  AddPlayerUpdate,
+  SetCellUpdate,
+  RemovePlayerUpdate,
 }
+
+export interface AddPlayerUpdate extends BaseUpdate {
+  type: UpdateType.AddPlayerUpdate,
+  id: PlayerId,
+  name: string,
+  colour: string,
+}
+
+export interface SetCellUpdate extends BaseUpdate {
+  type: UpdateType.SetCellUpdate,
+  location: Location,
+  owner: PlayerId,
+  value: number,
+}
+
+export interface RemovePlayerUpdate extends BaseUpdate {
+  type: UpdateType.RemovePlayerUpdate,
+  id: PlayerId,
+}
+
+export const create = (): Game => ({
+  grid: [
+    {
+      n: [],
+      e: [],
+      s: [],
+      w: [],
+      ne: { previousOwner: null, owner: null, value: 0 },
+      se: { previousOwner: null, owner: null, value: 0 },
+      sw: { previousOwner: null, owner: null, value: 0 },
+      nw: { previousOwner: null, owner: null, value: 0 },
+    }
+  ],
+  players: [],
+  tick: 0,
+})
+
+const generateActivePlayerUpdates = (game: Game): Array<SetCellUpdate> =>
+  game.players
+    .filter(player => player.ticksInLocation > 0)
+    .map(player => ({
+      tick: game.tick + 1,
+      type: UpdateType.SetCellUpdate,
+      location: player.location,
+      owner: tickCell(game)(player.location).owner.id,
+      value: tickCell(game)(player.location).value,
+    }))
+
+const tickCell = (game: Game) => (location: Location): Cell => ({})
+
+const cell = (grid: Grid) => (location: Location): Cell =>
+  _if(isWallLocation(location))
+    (grid[location[0]][location[1]][location[2]])
+    (grid[location[0]][location[1]])
+
+const neighbours = (location: Location): [Location, Location, Location, Location] => []
--- src/Utils.ts
+++ src/Utils.ts
@@ -1,1 +1,3 @@
 export const pipeline = (fs: Array<(x: any) => any>) => (x: any): any => fs.reduce((y, f) => f(y), x)
+
+export const _if = (p: boolean) => (a: any) => (b: any): any => p ? a : b
